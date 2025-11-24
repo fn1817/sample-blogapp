103344702e276f385f3941e73b6023f4d4e79777
you <webmg71@gmail.com>
2025-11-24 20:57:47 +0900

    メール修正

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   # default from: xx = xxからメールを送信する
-  default from: 'from@example.com'
+  default from: 'sandboxa632faec6a7c4880b081ccaba8cef8b4.mailgun.org'
   # views > layouts > mailer.html.haml（HTMLでメール送付）とmailer.text.haml（テキストでメール送付）をテンプレートとしてメールを作成する
   layout 'mailer'
 end
