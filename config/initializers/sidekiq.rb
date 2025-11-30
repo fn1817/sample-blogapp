a62ae964cfecc7b16020e2f546cbea2edb39f297
you <webmg71@gmail.com>
2025-11-30 14:04:12 +0900

    Revert "SSL検証を"
    This reverts commit 58b76aba849b14b5e83cb803f5978d167082cc49.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,15 +2,9 @@ Sidekiq.configure_server do |config|
   # redisサーバの場所を指定（'herokuで使う場合', 'ローカルPC上で使う場合'）
   # ENVに「REDIS_URL」があればそちらを使い、なければ「redis://localhost:6379」を使う
   # herokuではlocalhostは使えないので、環境変数で設定できるようにしている
-  config.redis = {
-    url: ENV.fetch('REDIS_URL', 'redis://localhost:6379'),
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } # TLS 検証を無効化
-    }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379') }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {
-    url: ENV.fetch('REDIS_URL', 'redis://localhost:6379')
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } # TLS 検証を無効化
-    }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379')}
 end
